d043157d9612aa8839a2fbbb4230a0a0b02ccb94
Federico Roux <rouxfederico@gmail.com>
2025-11-18 10:04:34 -0300

    Running 2 parametrized threads (#7)
    Running 2 parallel `std::thread`s without any synchronization technique
    
    stack-info: PR: https://github.com/fgr-17/cxx-multithreading/pull/7, branch: fgr-17/stack/7

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,11 @@
 #include <thread>
 #include <string>
 
-void printMessage() {
+void print_message() {
   std::cout << "Hello from thread" << std::endl;
 }
 
-void countTo(const int n, const std::string& name) {
+void count_to(const int n, const std::string& name) {
   for(auto i = 1; i <= n; i++) {
     std::cout << name << ": " << i << std::endl;
   }
@@ -26,10 +26,13 @@ void countTo(const int n, const std::string& name) {
 
 int main() {
   const int thread_1_max = 6;
+  const int thread_2_max = 3;
   Common c;
 
-  std::thread t(countTo, thread_1_max, "thread-1");
-  t.join();
+  std::thread t1(count_to, thread_1_max, "thread-1");
+  std::thread t2(count_to, thread_2_max, "thread-2");
+  t1.join();
+  t2.join();
 
   std::cout << "Hello from main" << std::endl;
 
